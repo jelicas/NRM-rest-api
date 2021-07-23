9aad09f3afe4fbfc1254fc71e58e2c9117b052e1
Jelica <jelicastanojevicc@gmail.com>
2021-07-23 17:32:51 +0200

    <feat>: Added cors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-import { database } from "./db/index.js";
-//import { postModel } from "./db/models/post.js";
-import { postRoutes } from "./routes/postRoutes.js";
+import cors from 'cors';
+import express from 'express';
 
-import express from "express";
-import bodyParser from "body-parser";
+import { database } from './db/index.js';
+import { postRoutes } from './routes/postRoutes.js';
 
 database.initialize();
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.options('*', cors());
+app.use(cors());
 
 app.use('/', postRoutes);
 
 app.listen(3000, () => {
-  console.log("Up and running!");
+  console.log('Up and running!');
 });
